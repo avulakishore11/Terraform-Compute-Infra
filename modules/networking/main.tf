a3b4bf54b31d4947f2843e7ca19668fa15039d85
avulakishore11 <avulakishore99@gmail.com>
2026-06-02 11:56:14 -0400

    fix: add tags to private DNS zone, VNet link, and private endpoint to satisfy tag enforcement policy

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -46,6 +46,7 @@ resource "azurerm_subnet" "private_endpoint_subnet" {
 resource "azurerm_private_dns_zone" "storage_file_dns_zone" {
   name                = "privatelink.file.core.windows.net"
   resource_group_name = var.resource_group_name
+  tags                = var.tags
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "storage_file_dns_link" {
@@ -53,6 +54,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "storage_file_dns_link"
   resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.storage_file_dns_zone.name
   virtual_network_id    = azurerm_virtual_network.virtual_network.id
+  tags                  = var.tags
 }
 
 resource "azurerm_private_endpoint" "storage_account_private_endpoint" {
@@ -60,6 +62,7 @@ resource "azurerm_private_endpoint" "storage_account_private_endpoint" {
   resource_group_name = var.resource_group_name
   location            = var.location
   subnet_id           = azurerm_subnet.private_endpoint_subnet.id
+  tags                = var.tags
 
   private_service_connection {
     name                           = "psc-${var.storage_account_name}"
