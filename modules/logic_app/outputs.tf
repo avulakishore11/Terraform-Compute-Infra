c0fbb050a6dc2a035b11f3bfda068a7f37ea6708
avulakishore11 <avulakishore99@gmail.com>
2026-06-02 10:25:06 -0400

    output refrences got changed

--- modules/logic_app/outputs.tf
+++ modules/logic_app/outputs.tf
@@ -1,19 +1,19 @@
 output "app_service_plan_id" {
-  value = azurerm_service_plan.main.id
+  value = azurerm_service_plan.plan.id
 }
 
 output "app_service_plan_name" {
-  value = azurerm_service_plan.main.name
+  value = azurerm_service_plan.plan.name
 }
 
 output "logic_app_id" {
-  value = azurerm_logic_app_standard.main.id
+  value = azurerm_logic_app_standard.plan.id
 }
 
 output "logic_app_name" {
-  value = azurerm_logic_app_standard.main.name
+  value = azurerm_logic_app_standard.plan.name
 }
 
 output "default_hostname" {
-  value = azurerm_logic_app_standard.main.default_hostname
+  value = azurerm_logic_app_standard.plan.default_hostname
 }
--- modules/storage_account/outputs.tf
+++ modules/storage_account/outputs.tf
@@ -1,26 +1,26 @@
 output "id" {
   description = "Resource ID of the storage account"
-  value       = azurerm_storage_account.this.id
+  value       = azurerm_storage_account.storage_account.id
 }
 
 output "name" {
   description = "Name of the storage account"
-  value       = azurerm_storage_account.this.name
+  value       = azurerm_storage_account.storage_account.name
 }
 
 output "primary_blob_endpoint" {
   description = "Primary blob service endpoint URL"
-  value       = azurerm_storage_account.this.primary_blob_endpoint
+  value       = azurerm_storage_account.storage_account.primary_blob_endpoint
 }
 
 output "primary_access_key" {
   description = "Primary storage account access key"
-  value       = azurerm_storage_account.this.primary_access_key
+  value       = azurerm_storage_account.storage_account.primary_access_key
   sensitive   = true
 }
 
 output "primary_connection_string" {
   description = "Primary connection string for the storage account"
-  value       = azurerm_storage_account.this.primary_connection_string
+  value       = azurerm_storage_account.storage_account.primary_connection_string
   sensitive   = true
 }
