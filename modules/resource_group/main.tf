bda93724e80fa30d28383c88b06eab24c2546361
avulakishore11 <avulakishore99@gmail.com>
2026-06-02 11:33:55 -0400

    g

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -11,7 +11,7 @@ terraform {
   }
 }
 
-resource "azurerm_resource_group" "this" {
+resource "azurerm_resource_group" "resource_group" {
   name     = var.name
   location = var.location
   tags     = var.tags
--- modules/resource_group/outputs.tf
+++ modules/resource_group/outputs.tf
@@ -1,14 +1,14 @@
 output "name" {
   description = "Name of the Resource Group"
-  value       = azurerm_resource_group.this.name
+  value       = azurerm_resource_group.resource_group.name
 }
 
 output "id" {
   description = "Resource ID of the Resource Group"
-  value       = azurerm_resource_group.this.id
+  value       = azurerm_resource_group.resource_group.id
 }
 
 output "location" {
   description = "Location of the Resource Group"
-  value       = azurerm_resource_group.this.location
+  value       = azurerm_resource_group.resource_group.location
 }
